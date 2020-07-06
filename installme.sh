6fcc1109f27aa0659d2e5d09500289c0e169e9aa
zh4ck3r8080 <67901866+zh4ck3r8080@users.noreply.github.com>
2020-07-06 06:17:56 -0400

    Create installme.sh

---
+++ installme.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ping -c 1 -q google.com >&/dev/null;
+
+if [ $? -eq 0 ]; then
+
+	echo "[+] Grab a CUP of Coffee :D"
+	printf "\n"
+	sudo apt update -y
+	sudo apt install git -y
+	sudo apt install tor -y
+
+	sudo git clone https://github.com/Und3rf10w/kali-anonsurf.git
+
+	cd kali-anonsurf
+	sudo chmod +x installer.sh
+	sudo ./installer.sh
+	cd ..
+	sudo cp bin/toripbounce /usr/bin/
+	sudo cp bin/stip /usr/bin/
+	sudo rm -rf kali-anonsurf
+
+	printf "\n"
+	echo "[+] All Done Now you can run this with toripbounce commmand !"
+	printf "\n"
+	echo "Warning Please run sudo stip command after you stop toripbounce"
+else
+    echo "[-] Please connect with Internet Connection !"
+fi
